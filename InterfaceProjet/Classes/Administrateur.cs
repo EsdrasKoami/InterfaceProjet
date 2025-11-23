b764823961efd174b67ad41e7de780234d0efe61
liliakerem08 <kerenbombo08@gmail.com>
2025-11-23 13:16:46 -0500

    feat(domain): modélisation des entités métier Projet, Employe, Client et Assignation

---
+++ InterfaceProjet/Classes/Administrateur.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProjet.Classes
+{
+    internal class Administrateur
+    {
+         int  idAdmin;
+            string nomUtilisateur;
+           string motDePasse;
+        DateTime dateCreation;
+        DateTime dateDerniereConnexion;
+
+        public Administrateur(int idAdmin, string nomUtilisateur, string motDePasse, DateTime dateCreation, DateTime dateDerniereConnexion)
+        {
+            this.idAdmin = idAdmin;
+            this.nomUtilisateur = nomUtilisateur;
+            this.motDePasse = motDePasse;
+            this.dateCreation = dateCreation;
+            this.dateDerniereConnexion = dateDerniereConnexion;
+        }
+
+        public int IdAdmin { get => idAdmin; set => idAdmin = value; }
+        public string NomUtilisateur { get => nomUtilisateur; set => nomUtilisateur = value; }
+        public string MotDePasse { get => motDePasse; set => motDePasse = value; }
+        public DateTime DateCreation { get => dateCreation; set => dateCreation = value; }
+        public DateTime DateDerniereConnexion { get => dateDerniereConnexion; set => dateDerniereConnexion = value; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ InterfaceProjet/Classes/Assignation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProjet.Classes
+{
+    internal class Assignation
+    {
+        int idAssignation;
+        string MatriculeEmploye;
+         string numeroProjet;
+        decimal heureTravaillee;
+        decimal salaireApayer;
+        DateTime dateAssignation;
+
+        public Assignation(int idAssignation, string matriculeEmploye, string numeroProjet, decimal heureTravaillee, decimal salaireApayer, DateTime dateAssignation)
+        {
+            this.idAssignation = idAssignation;
+            MatriculeEmploye = matriculeEmploye;
+            this.numeroProjet = numeroProjet;
+            this.heureTravaillee = heureTravaillee;
+            this.salaireApayer = salaireApayer;
+            this.dateAssignation = dateAssignation;
+        }
+
+        public int IdAssignation { get => idAssignation; set => idAssignation = value; }
+        public string MatriculeEmploye1 { get => MatriculeEmploye; set => MatriculeEmploye = value; }
+        public string NumeroProjet { get => numeroProjet; set => numeroProjet = value; }
+        public decimal HeureTravaillee { get => heureTravaillee; set => heureTravaillee = value; }
+        public decimal SalaireApayer { get => salaireApayer; set => salaireApayer = value; }
+        public DateTime DateAssignation { get => dateAssignation; set => dateAssignation = value; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
